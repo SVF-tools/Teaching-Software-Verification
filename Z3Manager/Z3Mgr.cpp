89009b44278cea724287eafcd5919960865be2ef
jumormt <jumormt@163.com>
2023-06-08 13:35:16 +0800

    sync with SVF

--- Z3Manager/Z3Mgr.cpp
+++ Z3Manager/Z3Mgr.cpp
@@ -227,7 +227,7 @@ s32_t Z3SSEMgr::getGepOffset(const GepStmt* gep){
 
         /// Caculate the offset
         if(const SVFPointerType* pty = SVFUtil::dyn_cast<SVFPointerType>(type))
-            totalOffset += offset * gep->getLocationSet().getElementNum(pty->getPtrElementType());
+            totalOffset += offset * gep->getAccessPath().getElementNum(pty->getPtrElementType());
         else
             totalOffset +=  SymbolTableInfo::SymbolInfo()->getFlattenedElemIdx(type, offset); 
     }
