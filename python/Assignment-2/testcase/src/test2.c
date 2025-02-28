68f679ac494c592dbcbb72da39804675e80ce838
bjjwwang <bjjwwang@github.com>
2025-02-28 06:00:31 +0000

    Release python version Assignment2 and new Dockerfile

---
+++ python/Assignment-2/testcase/src/test2.c
@@ -0,0 +1,14 @@
+#include "stdbool.h"
+//ADDR, LOAD, STORE, CAST(COPY)
+extern void svf_assert(bool);
+
+void foo(int* p) {
+  *p = 1;
+}
+
+int main() {
+    int a = 0;
+    foo(&a);
+    foo(&a);
+    svf_assert(a == 1);
+}
